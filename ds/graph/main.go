19e698bf72c75497f375a83e40c931a293fefa83
Nibras <nibrasmn027@gmail.com>
2023-02-02 19:46:28 +0530

    graph traversal

--- ds/graph/main.go
+++ ds/graph/main.go
@@ -2,7 +2,61 @@ package main
 
 import "fmt"
 
-type Graph map[int][]int
+type QueueNode struct {
+	data string
+	next *QueueNode
+}
+
+type Queue struct {
+	front *QueueNode
+	rear  *QueueNode
+}
+
+func (q Queue) Print() {
+	if q.rear == nil {
+		fmt.Println("queue is empty")
+		return
+	}
+	temp := q.front
+	for temp != nil {
+		fmt.Printf("%s - \n", temp.data)
+		temp = temp.next
+	}
+}
+func (q *Queue) IsEmpty() bool {
+	if q.rear == nil && q.front == nil {
+		return false
+	}
+	return true
+}
+
+func (q *Queue) Enqueue(data string) {
+	newNode := &QueueNode{data, nil}
+	if q.rear == nil {
+		q.front = newNode
+		q.rear = newNode
+		return
+	}
+	q.rear.next = newNode
+	q.rear = newNode
+}
+
+func (q *Queue) Dequeue() *QueueNode {
+	if q.front == nil {
+		fmt.Println("queue empty")
+		return nil
+	} else if q.front == q.rear {
+		x := q.front
+		q.front = nil
+		q.rear = nil
+		return x
+	}
+	x := q.front
+	q.front = q.front.next
+	return x
+}
+
+type Graph map[string][]string
 
 func (g *Graph) print() {
 	for i, val := range *g {
@@ -10,12 +64,12 @@ func (g *Graph) print() {
 	}
 }
 
-func (g *Graph) addVertex(data int) {
-	x := []int{}
+func (g *Graph) addVertex(data string) {
+	x := []string{}
 	(*g)[data] = x
 }
 
-func (g *Graph) insert(vertex, edge int, isBidirectional bool) {
+func (g *Graph) insert(vertex, edge string, isBidirectional bool) {
 	_, ok := (*g)[vertex]
 	if !ok {
 		g.addVertex(vertex)
@@ -32,6 +86,34 @@ func (g *Graph) insert(vertex, edge int, isBidirectional bool) {
 
 func main() {
 	g := Graph{}
-	g.insert(3, 4, true)
+	g.insert("A", "B", true)
+	g.insert("A", "F", true)
+	g.insert("B", "F", true)
+	g.insert("B", "C", true)
+	g.insert("C", "E", true)
+	g.insert("C", "D", true)
+	g.insert("D", "E", true)
+	g.insert("E", "F", true)
 	g.print()
+	fmt.Println(g.BFS("A"))
+}
+
+func (g *Graph) BFS(start string) []string {
+	output := []string{}
+	visited := map[string]bool{}
+	q := Queue{}
+	q.Enqueue(start)
+	visited[start] = true
+	for q.IsEmpty() {
+		x := q.Dequeue()
+		output = append(output, x.data)
+		for _, val := range (*g)[x.data] {
+			_, ok := visited[val]
+			if !ok {
+				q.Enqueue(val)
+				visited[val] = true
+			}
+		}
+	}
+	return output
 }
